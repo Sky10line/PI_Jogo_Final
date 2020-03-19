3ffb416de3b97bf8de25caf969ca22cbf4b118c1
Sky10line <rodrigo_ryo@hotmail.com>
2020-03-19 04:58:10 -0300

    Alterei package para correto

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pi_jogo_final;
+package PI_Jogo_Final;
 /**
  *
  * @author rodrigoaoki
